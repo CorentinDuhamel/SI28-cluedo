e37c0f62ebda62a4fb10831499dad9c86a199b63
Maël Belval <mael.belval.pro@gmail.com>
2020-04-16 03:14:59 +0200

    keep o moving biding

--- binding.js
+++ binding.js
@@ -1,5 +1,17 @@
 $(window).on("load", displaybutton)
 
 $(function() {
+    $("#buttonsafari").click(cachersafari)
+    $("#buttonsafari").mouseout(cacherBoutonssafari)
+    $("#buttonsafari").mouseover(afficherBoutonssafari)
     
+    $("#element8").click(element8)
+
+    $("#buttonfacetime").click(cacherfacetime)
+    $("#buttonfacetime").mouseout(cacherBoutonsfb)
+    $("#buttonfacetime").mouseover(afficherBoutonsfb)
+
+    $("#buttonmail").click(cachermail)
+    $("#buttonmail").mouseout(cacherBoutonsmail)
+    $("#buttonmail").mouseover(afficherBoutonsmail)
 })
