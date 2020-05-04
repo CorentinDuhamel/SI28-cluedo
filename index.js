238575cfd4985fa2d7264ce938815062f935a107
Corentin <37273930+CorentinDuhamel@users.noreply.github.com>
2020-05-04 11:53:58 +0200

    couleurmessage bug fixes

--- index.js
+++ index.js
@@ -14,12 +14,13 @@ $(document).ready(function() {
 
 function couleurmessage(){
     for (let i = 1; i <= 10; i++) {
-        $("message" + i).css("backgroundColor", "#353f4a")
+        $("#message" + i).css({"backgroundColor" : "#662266"});
         $("#contenumessage" + i).hide()
     }
     
+    $(".msg").css({"backgroundColor" : "#353f4a"})
     n = $(this).attr("data-message-id")
-    $('#message' + n).css("backgroundColor", "#0068e1")
+    $(this).css({"backgroundColor" : "#0068e1"});
     $('#contenumessage' + n).show()
 }
 
