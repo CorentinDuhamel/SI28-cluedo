ec23488c425372ef1ea0b4912aedf56e129a0ef2
Maël Belval <mael.belval.pro@gmail.com>
2020-06-12 23:28:24 +0200

    Fix button safari and finder

--- binding.js
+++ binding.js
@@ -7,7 +7,7 @@ $(function() {
     $(".off-menu").hide()
     $(".shutdown").hide()
 
-    btn_names = ["buttonsafari", "buttonfacetime", "buttonmail", "buttonmessage", "buttonphotos", "buttoncalendrier", "buttoncontact"]
+    btn_names = ["buttonsafari", "buttonfacetime", "buttonmail", "buttonmessage", "buttonphotos", "buttoncalendrier", "buttoncontact", "buttonfinder"]
     for(let btn_name of btn_names) {
         $("#" + btn_name).click(clickCacher)
         $("#" + btn_name).mouseout(toggleBoutons)
